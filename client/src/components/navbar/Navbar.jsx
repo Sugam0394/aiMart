cc127edc7f683e6201c45b0646667acd702abf46
Sugam0394 <singhsugam348@gmail.com>
2026-02-17 20:00:42 +0530

    start

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './Navbar.css'
 
 function Navbar() {
   const { user } = useSelector((state) => state.auth);

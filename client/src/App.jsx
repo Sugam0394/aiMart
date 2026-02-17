0c2f51c8ee5b491841dc9ffd0fcecbb02ddc82bc
Sugam0394 <singhsugam348@gmail.com>
2026-02-17 18:58:39 +0530

    start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, }from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate }from "react-router-dom";
 import { useSelector } from "react-redux";
  import { getAccessToken } from "./utils/token.js";
  import "./App.css"

d30708cb1739e60ea99ca61c78c56289c709847d
Sugam0394 <singhsugam348@gmail.com>
2026-02-18 16:24:26 +0530

    start

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,21 @@
- import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Local development mein Google Login error fix karne ke liye
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
     proxy: {
-      '/api': 'http://localhost:3000', // apna backend port
+      '/api': {
+        target: 'http://localhost:3000', // apna backend port
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
-});
+}); 
 
